463f4085cc8db458bc1adbbeb64309ebb9030bbe
Kdotropez <lefevre.nicolas@orange.fr>
2025-08-30 13:05:12 +0200

    v3.10.30: Extension des heures de fin pour supporter les fermetures précoces (14h30-23h59)

--- src/components/steps/ShopConfig.jsx
+++ src/components/steps/ShopConfig.jsx
@@ -148,7 +148,7 @@ const ShopConfig = ({ shop, onConfigUpdate, onNext, onBack }) => {
             gridTemplateColumns: 'repeat(auto-fit, minmax(80px, 1fr))',
             gap: '8px'
           }}>
-            {['18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '23:59'].map(time => (
+                         {['14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '23:59'].map(time => (
               <Button
                 key={time}
                 onClick={() => handleConfigChange('endTime', time)}
